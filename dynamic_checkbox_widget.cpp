0f6db46bff284526be388e77a7f83e4814902196
ValeryStk <tor-tech@mail.ru>
2025-04-17 17:11:34 +0300

    Update dynamic_checkbox_widget.cpp

--- dynamic_checkbox_widget.cpp
+++ dynamic_checkbox_widget.cpp
@@ -23,27 +23,23 @@ void DynamicCheckboxWidget::onCheckboxStateChanged(QCheckBox* checkBox,
     if (!checkBox) return; // Защита от nullptr
         qDebug() << checkBox->text();
 
-        // Блокируем сигналы только если это необходимо
-        const bool wasBlocked = checkBox->blockSignals(true);
-        checkBox->setChecked(false);
-        checkBox->blockSignals(wasBlocked); // Восстанавливаем исходное состояние
 
-        /*int checkedCount = 0;
+        int checkedCount = 0;
             for (QCheckBox *checkbox : checkboxes) {
                 if (checkbox->isChecked()) {
                     checkedCount++;
                 }
             }
-        if (checkedCount >= maxChecked) {
-            changedCheckbox->blockSignals(true);
-            changedCheckbox->setChecked(false);
-            changedCheckbox->blockSignals(false);
+        if (checkedCount > maxChecked) {
+            checkBox->blockSignals(true);
+            checkBox->setChecked(false);
+            checkBox->blockSignals(false);
             return;
         }
         qDebug()<<"Checked order size: "<<checkedOrder.size();
-        if (changedCheckbox->isChecked()) {
-            checkedOrder.append(changedCheckbox);
+        if (checkBox->isChecked()) {
+            checkedOrder.append(checkBox);
         } else {
-            checkedOrder.removeOne(changedCheckbox);
-        }*/
+            checkedOrder.removeOne(checkBox);
+        }
 }
