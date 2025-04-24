983354c3d0e618e73f4af866347fdf9c0eb9d343
ValeryStk <tor-tech@mail.ru>
2025-04-24 11:35:48 +0300

    wip (dynamic checkboxes)

--- dynamic_checkbox_widget.cpp
+++ dynamic_checkbox_widget.cpp
@@ -1,5 +1,14 @@
 #include "dynamic_checkbox_widget.h"
 #include "QDebug"
+#include <algorithm>
+
+namespace  {
+bool isRangeValid(const QVector<int>& vec, const int low, const int max) {
+    return std::all_of(vec.begin(), vec.end(), [low,max](int value) {
+        return value >= low && value <= max;
+    });
+}
+}
 
 DynamicCheckboxWidget::DynamicCheckboxWidget(const QList<QString>& labels,
                                              QWidget *parent,
@@ -8,7 +17,7 @@ DynamicCheckboxWidget::DynamicCheckboxWidget(const QList<QString>& labels,
 
     for (const QString& label : labels) {
         QCheckBox *checkbox = new QCheckBox(label, this);
-        checkbox->setIcon(QIcon(":/res/black.svg"));
+        checkbox->setIcon(QIcon(RES_COLOR_BLACK));
         connect(checkbox, &QCheckBox::toggled, this, [this,checkbox](){
              onCheckboxStateChanged(checkbox);
         });
@@ -31,31 +40,20 @@ QVector<QPair<int,int>> DynamicCheckboxWidget::get_choosed_bands()
 
 void DynamicCheckboxWidget::setInitialCheckBoxesToggled(const QVector<int> &toToggle)
 {
-    used_colors[0] = true;
-    used_colors[1] = true;
-    used_colors[2] = true;
-    if(toToggle.size()!=3)return;
-
-    checkboxes[toToggle[0]]->blockSignals(true);
-    checkboxes[toToggle[1]]->blockSignals(true);
-    checkboxes[toToggle[2]]->blockSignals(true);
-
-    checkboxes[toToggle[0]]->setChecked(true);
-    checkboxes[toToggle[1]]->setChecked(true);
-    checkboxes[toToggle[2]]->setChecked(true);
-
-    checkboxes[toToggle[0]]->setIcon(QIcon(":/res/blue.svg"));
-    checkboxes[toToggle[1]]->setIcon(QIcon(":/res/green.svg"));
-    checkboxes[toToggle[2]]->setIcon(QIcon(":/res/red.svg"));
-
+    if(toToggle.size()!=RGB_NUMBER)return;
+    if(isRangeValid(toToggle,0,checkboxes.size()-1)==false){
+        qDebug()<<"индексы для начальной установки чекбоксов находятся вне допустимого диапазона";
+        return;
+    };
     checkedOrder = {{},{},{}};
-    checkedOrder[0] = {checkboxes[toToggle[0]],2};
-    checkedOrder[1] = {checkboxes[toToggle[1]],1};
-    checkedOrder[2] = {checkboxes[toToggle[2]],0};
-
-    checkboxes[toToggle[0]]->blockSignals(false);
-    checkboxes[toToggle[1]]->blockSignals(false);
-    checkboxes[toToggle[2]]->blockSignals(false);
+    for(int i=0;i<RGB_NUMBER;++i){
+        used_colors[i] = true;
+        checkboxes[toToggle[i]]->blockSignals(true);
+        checkboxes[toToggle[i]]->setChecked(true);
+        checkboxes[toToggle[i]]->setIcon(QIcon(colors_svg[i]));
+        checkedOrder[i] = {checkboxes[toToggle[i]],color_sequence[i]};
+        checkboxes[toToggle[i]]->blockSignals(false);
+    }
 }
 
 void DynamicCheckboxWidget::onCheckboxStateChanged(QCheckBox* checkBox) {
@@ -63,34 +61,33 @@ void DynamicCheckboxWidget::onCheckboxStateChanged(QCheckBox* checkBox) {
     if (!checkBox) return; // Защита от nullptr
         qDebug() << checkBox->text();
 
-
         int checkedCount = 0;
             for (QCheckBox *checkbox : qAsConst(checkboxes)) {
                 if (checkbox->isChecked()) {
                     checkedCount++;
                 }
             }
-        if (checkedCount > maxChecked) {
+        if (checkedCount > MAX_POSSIBLE_CHECKED) {
             checkBox->blockSignals(true);
             checkBox->setChecked(false);
             checkBox->blockSignals(false);
             return;
         }
-        //qDebug()<<"Checked order size: "<<checkedOrder.size();
+
         if (checkBox->isChecked()) {
             int color=0;
-            if(used_colors[0]==false){
-              checkBox->setIcon(QIcon(":/res/red.svg"));
-              used_colors[0]=true;
-              color=0;
-            }else if(used_colors[1]==false){
-              checkBox->setIcon(QIcon(":/res/green.svg"));
-              used_colors[1]=true;
-              color=1;
-            }else if(used_colors[2]==false){
-              checkBox->setIcon(QIcon(":/res/blue.svg"));
-              used_colors[2]=true;
-              color=2;
+            if(used_colors[RED]==false){
+              checkBox->setIcon(QIcon(RES_COLOR_RED));
+              used_colors[RED]=true;
+              color=RED;
+            }else if(used_colors[GREEN]==false){
+              checkBox->setIcon(QIcon(RES_COLOR_GREEN));
+              used_colors[GREEN]=true;
+              color=GREEN;
+            }else if(used_colors[BLUE]==false){
+              checkBox->setIcon(QIcon(RES_COLOR_BLUE));
+              used_colors[BLUE]=true;
+              color=BLUE;
             }
             checkedOrder.append({checkBox,color});
         } else {
@@ -103,7 +100,7 @@ void DynamicCheckboxWidget::onCheckboxStateChanged(QCheckBox* checkBox) {
                 }
             }
             checkedOrder.removeOne({checkBox,color});
-            checkBox->setIcon(QIcon(":/res/black.svg"));
+            checkBox->setIcon(QIcon(RES_COLOR_BLACK));
         }
         emit choosed_bands_changed();
 }
--- dynamic_checkbox_widget.h
+++ dynamic_checkbox_widget.h
@@ -7,6 +7,17 @@
 #include <QString>
 #include <QList>
 
+#define RGB_NUMBER 3
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+constexpr char RES_COLOR_RED[] = ":/res/red.svg";
+constexpr char RES_COLOR_GREEN[] = ":/res/green.svg";
+constexpr char RES_COLOR_BLUE[] = ":/res/blue.svg";
+constexpr char RES_COLOR_BLACK[] = ":/res/black.svg";
+
 class DynamicCheckboxWidget : public QWidget {
     Q_OBJECT
 public:
@@ -27,8 +38,10 @@ signals:
 private:
     QList<QCheckBox*> checkboxes;
     QList<QPair<QCheckBox*,int>> checkedOrder; // Список для отслеживания порядка выбора чекбоксов
-    const int maxChecked = 3;
-    bool used_colors[3] = {false,false,false};//RGB
+    const int MAX_POSSIBLE_CHECKED = RGB_NUMBER;
+    bool used_colors[RGB_NUMBER] = {false,false,false};//RGB
+    QString colors_svg[RGB_NUMBER] = {RES_COLOR_BLUE,RES_COLOR_GREEN,RES_COLOR_RED};
+    int color_sequence[RGB_NUMBER] = {BLUE,GREEN,RED};
 };
 
 #endif // DYNAMIC_CHECKBOX_WIDGET_H
