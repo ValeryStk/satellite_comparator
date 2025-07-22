7b6ce0cbdff517ee80b0f13d7f8ee37b57832e3b
ValeryStk <tor-tech@mail.ru>
2025-07-22 16:15:35 +0300

    Update text_constants.cpp

--- text_constants.cpp
+++ text_constants.cpp
@@ -2,7 +2,7 @@
 
 namespace satc{
 
-extern const char app_name[] = "СПО \"ЛЕСОПАТОЛОГИЯ\"";
+const char app_name[] = "СПО \"ЛЕСОПАТОЛОГИЯ\"";
 
 const char euclid_metrika[] = "Евклидова метрика";
 const char spectral_angle[] = "Спектральный угол";
@@ -12,12 +12,9 @@ const char message_changing_bands[] = "Изменение отображения
 const char action_send_sample_text[] = "Выбрать как образец\n для сравнения со спутниковыми данными";
 
 
-const char satellite_name_landsat_9[] = "LANDSAT 9";
-
-const char satellite_name_landsat_8[] = "LANDSAT 8";
-
-extern const char satellite_name_sentinel_2A[] = "SENTINEL-2A";
-
-extern const char satellite_name_sentinel_2B[] = "SENTINEL-2B";
+const char satellite_name_landsat_9[]   = "LANDSAT 9";
+const char satellite_name_landsat_8[]   = "LANDSAT 8";
+const char satellite_name_sentinel_2A[] = "SENTINEL-2A";
+const char satellite_name_sentinel_2B[] = "SENTINEL-2B";
 
 }
